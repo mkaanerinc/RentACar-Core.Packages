4ed392a990ee2293f9a7bd0ff498dda0742d4a61
Mustafa Kaan Erinç <74507542+mkaanerinc@users.noreply.github.com>
2024-01-12 15:13:50 +0300

    Exception Logging & MsSqlLogger

---
+++ Core.CrossCuttingConcerns/Serilog/Logger/MsSqlLogger.cs
@@ -0,0 +1,36 @@
+﻿using Core.CrossCuttingConcerns.Serilog.ConfigurationModels;
+using Core.CrossCuttingConcerns.Serilog.Messages;
+using Microsoft.Extensions.Configuration;
+using Serilog.Sinks.MSSqlServer;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Serilog.Logger;
+
+public class MsSqlLogger : LoggerServiceBase
+{
+    public MsSqlLogger(IConfiguration configuration)
+    {
+        MsSqlConfiguration logConfiguration =
+            configuration.GetSection("SeriLogConfigurations:MsSqlConfiguration").Get<MsSqlConfiguration>()
+            ?? throw new Exception(SerilogMessages.NullOptionsMessage);
+
+        MSSqlServerSinkOptions sinkOptions = new()
+        {
+            TableName = logConfiguration.TableName,
+            AutoCreateSqlDatabase = logConfiguration.AutoCreateSqlTable
+        };
+
+        ColumnOptions columnOptions = new();
+
+        global::Serilog.Core.Logger seriLogConfig = new LoggerConfiguration().WriteTo
+            .MSSqlServer(logConfiguration.ConnectionString, sinkOptions, columnOptions: columnOptions)
+            .CreateLogger();
+
+        Logger = seriLogConfig;
+    }
+}
